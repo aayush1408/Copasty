b41a7958f6a149abef880fa8d211a51547b19165
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-19 18:43:37 +0530

    path resolved

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
+const path = require('path')
 const clipboardy = require('clipboardy')
 
 const readFileOf = (pathOfFile)=>{
   try{
-   let data = fs.readFileSync(pathOfFile,'utf-8')
+   let data = fs.readFileSync(path.resolve(__dirname,pathOfFile),'utf-8')
    clipboardy.writeSync(data)
    console.log('Copied!!')
   }
