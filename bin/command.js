d4ab907ca0a01093a638ca279ea6df88b104a760
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-31 00:10:57 +0530

    refactored code

--- bin/command.js
+++ bin/command.js
@@ -5,7 +5,7 @@ const readFileOf = require('../index').readFileOf;
 const pasteContent = require('../index').pasteContent;
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   .description('Copy content to clipboard and paste it to a specific file.')
 
 program
