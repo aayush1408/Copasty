244fe06674618d494a7aba4163161f9bd487ae0c
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-30 23:55:14 +0530

    paste functionality  added

--- bin/command.js
+++ bin/command.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
-const program = require('commander')
-const readFileOf = require('../index')
+
+const program = require('commander');
+const readFileOf = require('../index').readFileOf;
+const pasteContent = require('../index').pasteContent;
+
 program
   .version('1.0.0')
-  .description('Copy content to clipboard and paste to a specific file')
+  .description('Copy content to clipboard and paste it to a specific file.')
 
 program
   .command('copy')
   .arguments('<pathOfFile>')
-  .description('Copies the contents of a file')
+  .description('Copies the contents of a file.')
   .action((pathOfFile) => {
-    readFileOf(pathOfFile)
-  })
-program.parse(process.argv)  
+    readFileOf(pathOfFile);
+  });
+
+program
+  .command('paste')
+  .arguments('<pathOfFile>')
+  .description('Paste the contents of the clipboard to a file.')
+  .action((pathOfFile) => {
+    pasteContent(pathOfFile);
+  });
+
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const fs = require('fs')
-const path = require('path')
-const clipboardy = require('clipboardy')
+const fs = require('fs');
+const path = require('path');
+const clipboardy = require('clipboardy');
 
 const readFileOf = (pathOfFile) => {
   try {
     let data = fs.readFileSync(pathOfFile, 'utf-8')
-    clipboardy.writeSync(data)
-    console.log('Copied!!')
+    clipboardy.writeSync(data);
+    console.log('Copied!!');
   }
   catch (error) {
-    console.log(pathOfFile)
-    console.log('This is not the correct path for the file.')
+    console.log(pathOfFile);
+    console.log('This is not the correct path for the file.');
   }
 }
 
-module.exports = readFileOf
+const pasteContent = (pathOfFile) => {
+  try {
+    let data = clipboardy.readSync();
+    fs.writeFileSync(pathOfFile, data, 'utf-8');
+    console.log('Pasted');
+  }
+  catch (error) {
+    console.log(pathOfFile);
+    console.log('This is not the correct path for the file.');
+  }
+}
+
+module.exports = {
+  readFileOf: readFileOf,
+  pasteContent: pasteContent
+}
